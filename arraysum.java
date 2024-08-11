13c0a44663e3d1f81a057f0862e601094a710b59
Sayeeda Waris <106420051+sayeedawaris@users.noreply.github.com>
2024-08-11 22:41:05 +0530

    Create arraysum.java

---
+++ arraysum.java
@@ -0,0 +1,13 @@
+// Sum of elements of an array
+
+class SumOfArray {
+    public static void main(String[] args) {
+        int x[]={2,5,10,1,0};
+        int sum=0;
+        for(int i: x)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
